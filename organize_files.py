224a39ed17280a7bbf6d7f13c60ca2d2c4acc6e9
Sushan <85663813+Sushan2589@users.noreply.github.com>
2024-11-01 08:38:58 +0545

    Update organize_files.py
    added more extension map

--- organize_files.py
+++ organize_files.py
@@ -5,18 +5,31 @@ from pathlib import Path
 def organize_files(directory):
     # Dictionary to map file extensions to folder names
     extension_map = {
-        # Images
-        '.jpg': 'Images', '.jpeg': 'Images', '.png': 'Images', '.gif': 'Images',
-        # # Documents
-        '.pdf': 'Documents', '.doc': 'Documents', '.docx': 'Documents', '.txt': 'Documents',
-        # # Audio
-        '.mp3': 'Audio', '.wav': 'Audio', '.flac': 'Audio',
-        # # Video
-        '.mp4': 'Videos', '.avi': 'Videos', '.mkv': 'Videos',
-        # # Archives
-        '.zip': 'Archives', '.rar': 'Archives', '.7z': 'Archives',
-        # Code
-        '.py': 'Code', '.js': 'Code', '.html': 'Code', '.css': 'Code',
+       '.doc': 'Text', '.docx': 'Text', '.pdf': 'Text', '.rtf': 'Text', '.tex': 'Text', '.wpd': 'Text', '.txt': 'Text',
+        '.aif': 'Audio', '.cda': 'Audio', '.mp3': 'Audio', '.mpa': 'Audio', '.ogg': 'Audio', '.wav': 'Audio',
+        '.wma': 'Audio', '.wpl': 'Audio', '.7z': 'Compressed files', '.arj': 'Compressed files',
+        '.deb': 'Compressed files', '.pkg': 'Compressed files', '.rar': 'Compressed files', '.rpm': 'Compressed files',
+        '.gz': 'Compressed files', '.z': 'Compressed files', '.zip': 'Compressed files', '.bin': 'Disc and media',
+        '.dmg': 'Disc and media', '.iso': 'Disc and media', '.toast': 'Disc and media', '.vcd': 'Disc and media',
+        '.csv': 'Data and database', '.dat': 'Data and database', '.db': 'Data and database',
+        '.dbf': 'Data and database', '.log': 'Data and database', '.mdb': 'Data and database',
+        '.sav': 'Data and database', '.sql': 'Data and database', '.tar': 'Data and database',
+        '.xml': 'Data and database', '.apk': 'Executable files', '.bat': 'Executable files', '.cgi': 'Executable files',
+        '.com': 'Executable files', '.exe': 'Executable files', '.gadget': 'Executable files',
+        '.jar': 'Executable files', '.msi': 'Executable files', '.py': 'Programming files', '.wsf': 'Executable files',
+        '.ai': 'Images', '.bmp': 'Images', '.gif': 'Images', '.ico': 'Images', '.jpeg': 'Images', '.jpg': 'Images',
+        '.png': 'Images', '.ps': 'Images', '.psd': 'Images', '.svg': 'Images', '.tif': 'Images', '.tiff': 'Images',
+        '.css': 'Internet related files', '.aspx': 'Internet related files', '.asp': 'Internet related files',
+        '.html': 'Internet related files', '.htm': 'Internet related files', '.js': 'Internet related files',
+        '.jsp': 'Internet related files', '.php': 'Internet related files', '.xhtml': 'Internet related files',
+        '.key': 'Presentations', '.odp': 'Presentations', '.pps': 'Presentations', '.ppt': 'Presentations',
+        '.pptx': 'Presentations', '.c': 'Programming files', '.pl': 'Programming files', '.class': 'Programming files',
+        '.cpp': 'Programming files', '.cs': 'Programming files', '.h': 'Programming files', '.lnk': 'Shortcuts',
+        '.java': 'Programming files', '.sh': 'Programming files', '.swift': 'Programming files',
+        '.vb': 'Programming files', '.ods': 'Spreadsheet files', '.xls': 'Spreadsheet files',
+        '.xlsm': 'Spreadsheet files', '.xlsx': 'Spreadsheet files', '.avi': 'Video', '.3g2': 'Video', '.3gp': 'Video',
+        '.flv': 'Video', '.h264': 'Video', '.m4v': 'Video', '.mkv': 'Video', '.mov': 'Video', '.mp4': 'Video',
+        '.mpg': 'Video', '.mpeg': 'Video', '.rm': 'Video', '.swf': 'Video', '.vob': 'Video', '.wmv ': 'Video'
     }
 
     # Convert directory to Path object
